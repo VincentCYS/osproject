7a50703c0f91212bf3476eaeb5a8400c15fe6343
VincentCYS <vincent77674@gmail.com>
2019-03-30 21:28:27 +0800

    RC

--- s3.c
+++ s3.c
@@ -4,27 +4,31 @@
 
 int main()
 {
-	FILE *outputFile;
 	// 8 to 21
 	// 19:00 to 22:00
-	int NUM_INPUT = 4;
-	int i, j, len;
-	char *algorithm[10];// used algorithm
-	char *eventType[30];// event type: assignment...
-	char *status[15];	// store accetped or rejected
-	char *strbuf[30];
+	int NUM = 4;
 	char *data[14][6] = { 
 		// event name	datetime			duration	priority	A/R		index
 		{ "COMP2432A1", "2019-04-18", 		"12", 		"2", 		"1", 	"1"}, 
 		{ "COMP2422P1", "2019-04-20", 		"26", 		"1", 		"1", 	"2"}, 
-		{ "COMP2000", 	"2019-04-14 19:00", "2", 		"3", 		"1", 	"3" },
-		{ "Meeting", 	"2019-04-18 20:00", "2", 		"4", 		"1", 	"4" }
+		{ "COMP2000", 	"2019-04-14-19:00", "2", 		"3", 		"1", 	"3" },
+		{ "Meeting", 	"2019-04-18-20:00", "2", 		"4", 		"1", 	"4" }
 	};
 
-	char *fileName[50];
-	strcpy(fileName, "S3_report_fcfs_01.dat");
-	strcpy(algorithm, "FCFS"); // can be removed after the merged
+	createLogFile(NUM, "S3_report_fcfs_01.dat", "FCFS", data);
 
+}
+
+
+
+createLogFile(int NUM_INPUT, char * fileName, char *algorithm, char *data[14][6]) {
+	FILE *outputFile;
+	char *algorithm[10];// used algorithm
+	char *eventType[30];// event type: assignment...
+	char *status[15];	// store accetped or rejected
+	char *strbuf[30];
+	char *fileName[50];
+	int i, j, len;
 
 	//printf("%s\n", fileName);
 	outputFile = fopen(fileName, "w");
@@ -98,5 +102,4 @@ int main()
 
 	printf("Log file created!!! \n");
 
-	//algorithm input:  Char *2dArray[][] = [event name, due date, duration, priority,accept(0 or 1, default 0), index ] 
 }
